4405b8f63ec6456e1fec1107dfe3e153419d1863
Yousef Mehnati <yousef.mehnati@domacare.fi>
2023-05-01 18:37:07 +0300

    added progress message when converting

--- utils/utils.js
+++ utils/utils.js
@@ -28,6 +28,33 @@ const convertFile = async (fileName, chatId, subtitleTrackId, audioTrackId, bot)
                 '-c:a', 'copy',
                 `./${fileName}.mp4`, // if you don't want to stream the data while converting, use this
             ]);
+
+            // Get the video duration
+            let videoDurationInSeconds = null;
+            videoConversion.stderr.on('data', (data) => {
+                const str = data.toString();
+                const match = str.match(/Duration:\s*([\d:.]+)/);
+                if (match) {
+                    const timeString = match[1];
+                    videoDurationInSeconds = convertTimeStringToSeconds(timeString);
+                }
+            });
+
+            // Update progress percentage
+            videoConversion.stderr.on('data', (data) => {
+                const str = data.toString();
+                const match = str.match(/time=(\d{2}):(\d{2}):(\d{2})\.(\d{2})/);
+                if (match && videoDurationInSeconds) {
+                    const hours = parseInt(match[1], 10);
+                    const minutes = parseInt(match[2], 10);
+                    const seconds = parseInt(match[3], 10);
+                    const currentTimeInSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+                    const percentage = Math.floor((currentTimeInSeconds / videoDurationInSeconds) * 100);
+                    // bot.sendMessage(chatId, `File conversion progress: ${percentage}%`);
+                    updateProgress(chatId, percentage, bot)
+                }
+            });
+
             videoConversion.on('close', (c) => {
                 if(c === 0) {
                     bot.sendVideo(chatId, `./${fileName}.mp4`).then(() => {
@@ -40,6 +67,31 @@ const convertFile = async (fileName, chatId, subtitleTrackId, audioTrackId, bot)
         }
     })
 
+}
+
+let progressMessageId; // variable to store the ID of the progress message
+const updateProgress = (chatId, percentage, bot) => {
+    const message = `File conversion progress: ${percentage}`;
+    console.log(progressMessageId)
+    if (progressMessageId) {
+        // if progress message already sent, update it
+        bot.editMessageText(message, { chat_id: chatId, message_id: progressMessageId }).then((editedMessage) => {
+            console.log(editedMessage.text)
+        });
+    } else {
+        // if progress message not sent, send new message and store the message ID
+        bot.sendMessage(chatId, message, { parse_mode: 'HTML' }).then((sentMessage) => {
+            progressMessageId = sentMessage.message_id;
+        });
+    }
+};
+
+const convertTimeStringToSeconds = (timeString) => {
+    const parts = timeString.split(':');
+    const hours = parseInt(parts[0], 10);
+    const minutes = parseInt(parts[1], 10);
+    const seconds = parseFloat(parts[2], 10);
+    return (hours * 60 * 60) + (minutes * 60) + seconds;
 }
 
 const deleteFile = (filePath) => {
